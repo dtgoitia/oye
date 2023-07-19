107b563f2ef2374966cdb0c7f793263f60d5ae25
David Torralba Goitia <david.torralba.goitia@gmail.com>
2023-07-20 00:29:54 +0200

    WIP: add support for 'at 9pm' utterances

--- api/src/domain/inference.py
+++ api/src/domain/inference.py
@@ -2,12 +2,26 @@ import datetime
 import enum
 import re
 
-from src.domain.reminders import NewReminder, Once, Schedule
+from src.devex import UnexpectedScenario, todo
+from src.domain.reminders import NewReminder, Occurrence, Once, Schedule
 from src.exhaustive_match import assert_never
 from src.model import Utterance
 
+STARTS_WITH_IN = re.compile(r"^in (?P<amount>[0-9]+)\s*(?P<unit>[a-zA-Z]+) (?P<message>.*)$")
 ENDS_WITH_IN = re.compile(r"^(?P<message>.*) in (?P<amount>[0-9]+)\s*(?P<unit>[a-zA-Z]+)$")
+STARTS_WITH_AT = re.compile(r"^at (?P<when>[0-9\.\:\sampAMP]+) (?P<message>.*)$")
 ENDS_WITH_AT = re.compile(r"^(?P<message>.*) at (?P<when>[0-9\.\:\sampAMP]+)$")
+
+# ======================================================================================
+# RegEx patterns to parse 'at ...'
+HOUR_PATTERN = r"(?P<hour>[0-2]?[0-9])"
+HOUR_AND_MIN_SEPARATOR = r"(\s|\:|\.)"
+MINS_PATTERN = r"(?P<min>[0-5][0-9])"
+AM_PM_PATTERN = r"(?P<am_pm>([ap]m|[AP]M))"
+PARSE_TIME_PATTERN = re.compile(
+    rf"^{HOUR_PATTERN}{HOUR_AND_MIN_SEPARATOR}?{MINS_PATTERN}?\s?{AM_PM_PATTERN}?"
+)
+# ======================================================================================
 
 
 class AmountMustBeNumeric(Exception):
@@ -72,7 +86,7 @@ def _infer_starts_with_in_x_time(raw: str) -> tuple[str, Once]:
     """
     Return the Schedule for an utterance like 'in 3 mins ...'.
     """
-    ...
+    todo()
 
 
 def _infer_ends_with_in_x_time(raw: str) -> tuple[str, Once]:
@@ -92,11 +106,48 @@ def _infer_ends_with_in_x_time(raw: str) -> tuple[str, Once]:
     return message, schedule
 
 
+def start_of_today() -> datetime.datetime:
+    today = datetime.date.today()
+    return datetime.datetime(year=today.year, month=today.month, day=today.day)
+
+
+def _infer_at_time(raw: str) -> Occurrence:
+    match = PARSE_TIME_PATTERN.match(raw)
+    if not match:
+        raise InferenceFailed(f"unsupported time pattern: {raw!r}")
+
+    hour = match.group("hour")
+    min = match.group("min") or 0
+    am_pm = match.group("am_pm")
+
+    h = int(hour)
+    m = int(min)
+    match am_pm:
+        case None | "am" | "AM":
+            h += 0
+        case "pm" | "PM":
+            h += 12
+        case _:
+            raise UnexpectedScenario(f"expected AM/PM, but got {am_pm!r} instead")
+
+    at = start_of_today() + datetime.timedelta(hours=h, minutes=m)
+
+    return at
+
+
 def _infer_start_with_at_x(raw: str) -> tuple[str, Once]:
     """
     Return the Schedule for an utterance like 'at 8.32am ...'.
     """
-    ...
+    match = STARTS_WITH_AT.match(raw)
+    if not match:
+        raise InferenceFailed(f"expected to start with 'at <time_pattern>', but got {raw} instead")
+
+    message = match.group("message")
+    when = match.group("when")
+
+    at = _infer_at_time(raw=when)
+    return message, Once(at=at)
 
 
 def _infer_ends_with_at_x(raw: str) -> tuple[str, Once]:
@@ -111,6 +162,9 @@ def _infer_ends_with_at_x(raw: str) -> tuple[str, Once]:
     message = match.group("message")
     when = match.group("when")
 
+    at = _infer_at_time(raw=when)
+    return message, Once(at=at)
+
 
 def _infer_schedule(utterance: Utterance) -> tuple[str, Schedule]:
     """
@@ -118,10 +172,10 @@ def _infer_schedule(utterance: Utterance) -> tuple[str, Schedule]:
 
     The interpreter is timezone aware.
     """
-    try:
-        return _infer_starts_with_in_x_time(raw=utterance)
-    except InferenceFailed:
-        ...
+    # try:
+    #     return _infer_starts_with_in_x_time(raw=utterance)
+    # except InferenceFailed:
+    #     ...
 
     try:
         return _infer_ends_with_in_x_time(raw=utterance)
@@ -137,6 +191,8 @@ def _infer_schedule(utterance: Utterance) -> tuple[str, Schedule]:
         return _infer_ends_with_at_x(raw=utterance)
     except InferenceFailed:
         ...
+
+    raise InferenceFailed(f"Could not infer schedule from {utterance!r}")
 
     # yesterday = datetime.datetime.today() - datetime.timedelta(days=1)
     # tomorrow = datetime.datetime.today() + datetime.timedelta(days=1)
--- api/tests/domain/test_inference.py
+++ api/tests/domain/test_inference.py
@@ -1,23 +1,60 @@
 from datetime import datetime
+
 import pytest
 
 from src.domain.inference import infer_reminder
 from src.domain.reminders import Once
 from tests.factories import d
 
-@pytest.mark.freeze_time("2020-02-03 04:05:06Z")
+
+@pytest.mark.freeze_time("2020-02-03 01:01:06Z")
 @pytest.mark.parametrize(
     ("raw", "expected"),
     (
-        ("9 foo", midnight + datetime.timedelta(hours=9, minutes=0)),
-        ("17 foo", midnight + datetime.timedelta(hours=17, minutes=0)),
-        ("5am foo", midnight + datetime.timedelta(hours=5, minutes=0)),
-        ("5pm foo", midnight + datetime.timedelta(hours=17, minutes=0)),
-        ("5AM foo", midnight + datetime.timedelta(hours=5, minutes=0)),
-        ("5PM foo", midnight + datetime.timedelta(hours=17, minutes=0)),
-    )
+        # only hours
+        ("at 9", "2020-02-03 09:00:00"),
+        ("at 17", "2020-02-03 17:00:00"),
+        ("at 5am", "2020-02-03 05:00:00"),
+        ("at 5pm", "2020-02-03 17:00:00"),
+        ("at 5AM", "2020-02-03 05:00:00"),
+        ("at 5PM", "2020-02-03 17:00:00"),
+        # # with ':' separator
+        ("at 9:01", "2020-02-03 09:01:00"),
+        ("at 9:15", "2020-02-03 09:15:00"),
+        ("at 17:23", "2020-02-03 17:23:00"),
+        ("at 5:11am", "2020-02-03 05:11:00"),
+        ("at 5:42pm", "2020-02-03 17:42:00"),
+        ("at 5:42PM", "2020-02-03 17:42:00"),
+        ("at 5:11AM", "2020-02-03 05:11:00"),
+        # # with '.' separator
+        ("at 9.01", "2020-02-03 09:01:00"),
+        ("at 9.15", "2020-02-03 09:15:00"),
+        ("at 17.23", "2020-02-03 17:23:00"),
+        ("at 5.11am", "2020-02-03 05:11:00"),
+        ("at 5.42pm", "2020-02-03 17:42:00"),
+        ("at 5.42PM", "2020-02-03 17:42:00"),
+        ("at 5.11AM", "2020-02-03 05:11:00"),
+        # # with ' ' separator
+        ("at 9 01", "2020-02-03 09:01:00"),
+        ("at 9 15", "2020-02-03 09:15:00"),
+        ("at 17 23", "2020-02-03 17:23:00"),
+        ("at 5 11am", "2020-02-03 05:11:00"),
+        ("at 5 42pm", "2020-02-03 17:42:00"),
+        ("at 5 42PM", "2020-02-03 17:42:00"),
+        ("at 5 11AM", "2020-02-03 05:11:00"),
+    ),
 )
-def test_infer_time_only_hour(raw: str, expected: datetime) -> None
+def test_infer_at_time(raw: str, expected: datetime) -> None:
+    expected_occurrence = d(expected)
+
+    reminder = infer_reminder(utterance=f"{raw} do foo")
+    assert reminder.description == "do foo"
+    assert reminder.schedule.at == expected_occurrence
+
+    reminder = infer_reminder(utterance=f"do foo {raw}")
+    assert reminder.description == "do foo"
+    assert reminder.schedule.at == expected_occurrence
+
 
 @pytest.mark.freeze_time("2020-02-03 04:05:06Z")
 @pytest.mark.parametrize(
