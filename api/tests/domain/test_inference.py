099cc6fd1d05a85ca491fee600bec46ac2d34b42
David Torralba Goitia <david.torralba.goitia@gmail.com>
2023-07-20 00:29:54 +0200

    WIP: add support for 'at 9pm' utterances

--- api/tests/domain/test_inference.py
+++ api/tests/domain/test_inference.py
@@ -1,93 +1,82 @@
-from datetime import datetime
 import pytest
 
 from src.domain.inference import infer_reminder
 from src.domain.reminders import Once
 from tests.factories import d
 
-@pytest.mark.freeze_time("2020-02-03 04:05:06Z")
-@pytest.mark.parametrize(
-    ("raw", "expected"),
-    (
-        ("9 foo", midnight + datetime.timedelta(hours=9, minutes=0)),
-        ("17 foo", midnight + datetime.timedelta(hours=17, minutes=0)),
-        ("5am foo", midnight + datetime.timedelta(hours=5, minutes=0)),
-        ("5pm foo", midnight + datetime.timedelta(hours=17, minutes=0)),
-        ("5AM foo", midnight + datetime.timedelta(hours=5, minutes=0)),
-        ("5PM foo", midnight + datetime.timedelta(hours=17, minutes=0)),
-    )
-)
-def test_infer_time_only_hour(raw: str, expected: datetime) -> None
 
-@pytest.mark.freeze_time("2020-02-03 04:05:06Z")
+@pytest.mark.freeze_time("2020-02-03 01:01:06Z")
 @pytest.mark.parametrize(
     ("raw", "expected"),
     (
-        pytest.param(
-            "do stuff in 1 m",
-            Once(at=d("2020-02-03 04:06:06Z")),
-            id="in_1_m_with_space",
-        ),
-        pytest.param(
-            "do stuff in 1m",
-            Once(at=d("2020-02-03 04:06:06Z")),
-            id="in_1_m_without_space",
-        ),
-        pytest.param(
-            "do stuff in 1 min",
-            Once(at=d("2020-02-03 04:06:06Z")),
-            id="in_1_min_with_space",
-        ),
-        pytest.param(
-            "do stuff in 1min",
-            Once(at=d("2020-02-03 04:06:06Z")),
-            id="in_1_min_without_space",
-        ),
-        pytest.param(
-            "do stuff in 2 mins",
-            Once(at=d("2020-02-03 04:07:06Z")),
-            id="in_2_mins_with_space",
-        ),
-        pytest.param(
-            "do stuff in 2mins",
-            Once(at=d("2020-02-03 04:07:06Z")),
-            id="in_2_mins_without_space",
-        ),
-        pytest.param(
-            "do stuff in at 9pm",
-            Once(at=d("2020-02-03 21:00:00Z")),
-            id="at_9_pm_without_space",
-        ),
-        pytest.param(
-            "do stuff in at 9 pm",
-            Once(at=d("2020-02-03 21:00:00Z")),
-            id="at_9_pm_with_space",
-        ),
-        pytest.param(
-            "do stuff in at 9.00pm",
-            Once(at=d("2020-02-03 21:00:00Z")),
-            id="at_900_pm_with_dot_and_without_space",
-        ),
-        pytest.param(
-            "do stuff in at 9.00 pm",
-            Once(at=d("2020-02-03 21:00:00Z")),
-            id="at_900_pm_with_dot_and_with_space",
-        ),
-        pytest.param(
-            "do stuff in at 9:00pm",
-            Once(at=d("2020-02-03 21:00:00Z")),
-            id="at_900_pm_with_colon_and_without_space",
-        ),
-        pytest.param(
-            "do stuff in at 9:00 pm",
-            Once(at=d("2020-02-03 21:00:00Z")),
-            id="at_900_pm_with_colon_and_with_space",
-        ),
+        # only hours
+        ("at 9", "2020-02-03 09:00:00"),
+        ("at 17", "2020-02-03 17:00:00"),
+        ("at 5am", "2020-02-03 05:00:00"),
+        ("at 5pm", "2020-02-03 17:00:00"),
+        ("at 5AM", "2020-02-03 05:00:00"),
+        ("at 5PM", "2020-02-03 17:00:00"),
+        # # with ':' separator
+        ("at 9:01", "2020-02-03 09:01:00"),
+        ("at 9:15", "2020-02-03 09:15:00"),
+        ("at 17:23", "2020-02-03 17:23:00"),
+        ("at 5:11am", "2020-02-03 05:11:00"),
+        ("at 5:42pm", "2020-02-03 17:42:00"),
+        ("at 5:42PM", "2020-02-03 17:42:00"),
+        ("at 5:11AM", "2020-02-03 05:11:00"),
+        # # with '.' separator
+        ("at 9.01", "2020-02-03 09:01:00"),
+        ("at 9.15", "2020-02-03 09:15:00"),
+        ("at 17.23", "2020-02-03 17:23:00"),
+        ("at 5.11am", "2020-02-03 05:11:00"),
+        ("at 5.42pm", "2020-02-03 17:42:00"),
+        ("at 5.42PM", "2020-02-03 17:42:00"),
+        ("at 5.11AM", "2020-02-03 05:11:00"),
+        # # with ' ' separator
+        ("at 9 01", "2020-02-03 09:01:00"),
+        ("at 9 15", "2020-02-03 09:15:00"),
+        ("at 17 23", "2020-02-03 17:23:00"),
+        ("at 5 11am", "2020-02-03 05:11:00"),
+        ("at 5 42pm", "2020-02-03 17:42:00"),
+        ("at 5 42PM", "2020-02-03 17:42:00"),
+        ("at 5 11AM", "2020-02-03 05:11:00"),
     ),
 )
-def test_infer(raw: str, expected: Once) -> None:
-    reminder = infer_reminder(utterance=raw)
+def test_infer_at_time(raw: str, expected: str) -> None:
+    expected_occurrence = d(expected)
 
+    reminder = infer_reminder(utterance=f"{raw} do foo")
     assert isinstance(reminder.schedule, Once)
-    assert reminder.description == "do stuff"
-    assert reminder.schedule.at == expected.at
+    assert reminder.description == "do foo"
+    assert reminder.schedule.at == expected_occurrence
+
+    reminder = infer_reminder(utterance=f"do foo {raw}")
+    assert isinstance(reminder.schedule, Once)
+    assert reminder.description == "do foo"
+    assert reminder.schedule.at == expected_occurrence
+
+
+@pytest.mark.freeze_time("2020-02-03 04:05:06Z")
+@pytest.mark.parametrize(
+    ("raw", "expected"),
+    (
+        ("in 1 m", "2020-02-03 04:06:06Z"),
+        ("in 1m", "2020-02-03 04:06:06Z"),
+        ("in 1 min", "2020-02-03 04:06:06Z"),
+        ("in 1min", "2020-02-03 04:06:06Z"),
+        ("in 2 mins", "2020-02-03 04:07:06Z"),
+        ("in 2mins", "2020-02-03 04:07:06Z"),
+    ),
+)
+def test_infer_in_time(raw: str, expected: str) -> None:
+    expected_occurrence = d(expected)
+
+    reminder = infer_reminder(utterance=f"{raw} do foo")
+    assert isinstance(reminder.schedule, Once)
+    assert reminder.description == "do foo"
+    assert reminder.schedule.at == expected_occurrence
+
+    reminder = infer_reminder(utterance=f"do foo {raw}")
+    assert isinstance(reminder.schedule, Once)
+    assert reminder.description == "do foo"
+    assert reminder.schedule.at == expected_occurrence
